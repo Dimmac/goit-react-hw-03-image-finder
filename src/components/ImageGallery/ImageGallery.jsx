3e820a7ec97653db684799504c6683bcb3f44f91
Dimmac <dmytro.lojivsky@gmail.com>
2022-02-04 00:59:16 +0200

    add_ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,10 +1,26 @@
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem'
-import {ImageGalleryStyle} from './ImageGallery.styled'
-const ImageGallery = () => {
-    return (<><h1>ImageGallery component</h1>
-    <ImageGalleryStyle>
-        <ImageGalleryItem />
-</ImageGalleryStyle>
-    </>)
-}
-export default ImageGallery
+import {ImageGallery} from './ImageGallery.styled'
+import PropTypes from 'prop-types';
+
+const ImageGalleryPictures = ({ picture, onClick }) => {
+  return (
+    <ImageGallery>
+      {picture.map(({ largeImageURL, webformatURL, tags }, index) => {
+        return (
+          <ImageGalleryItem
+            key={index}
+            data={{ largeImageURL, webformatURL, tags }}
+            largePicture={onClick}
+          />
+        );
+      })}
+    </ImageGallery>
+  );
+};
+
+export default ImageGalleryPictures;
+
+ImageGalleryPictures.propTypes = {
+  picture: PropTypes.arrayOf(PropTypes.shape).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
